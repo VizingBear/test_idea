71e04c446cb0d8489392f292a680f94f60adcbae
VizingBear <141499516+VizingBear@users.noreply.github.com>
2024-03-19 23:55:21 +0700

    Update urls.py

--- testyuo/urls.py
+++ testyuo/urls.py
@@ -3,6 +3,6 @@ from .views import test_view, test_view_final
 from django.urls import path
 
 urlpatterns = [
-    path('test', test_view),
+    path('test', test_view), тестовый путь на котором есть форма отправки
     path('test2', test_view_final, name='test_final'),
 ]
